2c63aef2159efe76ac6c66839f317be4825d8679
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-06-08 23:52:32 -0500

    initial functionality complete. vague theming

--- app.py
+++ app.py
@@ -1,11 +1,71 @@
-import flask
+from flask import flash, Flask, redirect, render_template, request
+from character_model import Character
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+
+with open("secretkey.txt", "r") as f:
+    app.secret_key = f.readlines()[0]
 
 print("started!")
 
 @app.route("/")
 def index():
-    return "Hello world!"
+    return redirect("/characters")
+
+@app.route("/characters")
+def characters():
+    search_term = request.args.get("q")
+    if search_term is not None:
+        characters_set = Character.search(search_term)
+    else:
+        characters_set = Character.all()
+    return render_template("index.html", characters=characters_set)
+
+@app.route("/characters/new", methods=['GET'])
+def characters_new_get():
+    return render_template("new.html", character=Character())
+
+@app.route("/characters/new", methods=['POST'])
+def characters_new_post():
+    c = Character(  None,
+                    request.form['name'],
+                    request.form['caste'],
+                    request.form['descriptor'],
+                    request.form['gift'])
+    if c.save():
+        flash("Created new character!")
+        return redirect("/characters")
+    else:
+        return render_template("new.html", character=c)
+    
+@app.route("/characters/<id>")
+def character_view(id=0):
+    character = Character.find(id)
+    return render_template("show.html", character=character)
+
+@app.route("/characters/<id>/edit", methods=['GET'])
+def character_edit_get(id=0):
+    character = Character.find(id)
+    return render_template("edit.html", character=character)
+
+@app.route("/characters/<id>/edit", methods=['POST'])
+def character_edit_post(id=0):
+    c = Character.find(id)
+    c.update(request.form['name'],
+             request.form['caste'],
+             request.form['descriptor'],
+             request.form['gift'])
+    if c.save():
+        flash("Updated character!")
+        return redirect("/characters/" + str(id) )
+    else:
+        return render_template("edit.html", character=c)
+
+@app.route("/characters/<id>/delete", methods=['POST'])
+def character_delete(id=0):
+    character = Character.find(id)
+    character.delete()
+    flash("Deleted Character!")
+    return redirect("/characters")
 
 app.run()
---
+++ character_model.py
@@ -0,0 +1,81 @@
+import json
+
+PAGE_SIZE = 100
+
+class Character:
+
+    db = {}
+
+    def __init__(self, id=None, name=None, caste=None, descriptor=None, gift=None):
+        self.id = id
+        self.name = name
+        self.caste = caste
+        self.descriptor = descriptor
+        self.gift = gift
+        self.errors = {}
+
+    def delete(self):
+        del Character.db[self.id]
+        Character.save_db()
+
+    def save(self):
+        if not self.validate():
+            return False
+        if self.id is None:
+            if len(Character.db) == 0:
+                max_id = 0
+            else:
+                max_id = max(character.id for character in Character.db.values())
+            self.id = max_id + 1
+            Character.db[self.id] = self
+        Character.save_db()
+        return True
+    
+    def update(self, name, caste, descriptor, gift):
+        self.name = name
+        self.caste = caste
+        self.descriptor = descriptor
+        self.gift = gift
+
+    def validate(self):
+        if not self.gift:
+            self.errors['gift'] = "Gift Required"
+        existing_character = next(filter(
+                lambda c: c.id != self.id and c.gift == self.gift,
+                Character.db.values()),
+            None)
+        if existing_character:
+            self.errors['gift'] = "Gift Must Be Unique"
+        return len(self.errors) == 0
+    
+    @classmethod
+    def all(cls, page=1):
+        page = int(page)
+        start = (page - 1) * PAGE_SIZE
+        end = start + PAGE_SIZE
+        return list(cls.db.values())[start:end]
+
+    @classmethod
+    def find(cls, id):
+        id = int(id)
+        c = cls.db.get(id)
+        if c is not None:
+            c.errors = {}
+        return c
+    @classmethod    
+    def search(cls, search_term):
+        results = []
+        for c in cls.db.values():
+            match_name = c.name is not None and search_term in c.name
+            match_caste = c.caste is not None and search_term in c.caste
+            match_gift = c.gift is not None and search_term in c.gift
+            match_descriptor = c.descriptor is not None and search_term in c.descriptor
+            if match_name or match_caste or match_gift or match_descriptor:
+                results.append(c)
+        return results
+
+    @staticmethod
+    def save_db():
+        out_arr = [c.__dict__ for c in Character.db.values()]
+        with open("characters.json", "w") as f:
+            json.dump(out_arr, f, indent=2)
